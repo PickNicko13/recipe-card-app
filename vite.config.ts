bd2ccac72a86264bede26530ac8f4084a5195a89
Mykyta <picknicko13@gmail.com>
2026-03-30 16:30:44 +0200

    Set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages deployment
+  base: '/recipe-card-app/', // For GitHub Pages deployment
   build: {
     outDir: 'dist',
     sourcemap: true,
